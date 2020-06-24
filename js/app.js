c32db6d9f074f7bce6703a509b8f0ae9bbba68e5
Claire Tregunna <c.tregunna1980@gmail.com>
2020-06-24 16:26:14 +0100

    test for modal function

--- js/app.js
+++ js/app.js
@@ -2,3 +2,12 @@ let today = new Date();
 let year = today.getFullYear();
 let currentYear = document.querySelector('#date');
 currentYear.innerHTML = year;
+
+
+// Test to check modal works - to be removed
+const showModal = document.querySelector('#show');
+
+showModal.addEventListener('click', e => {
+    let modal = document.querySelector('.overlay');
+    modal.classList.remove('hidden');
+});
