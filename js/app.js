c68bc49ddeb4049806c2079e582e12028c12baf1
Claire Tregunna <c.tregunna1980@gmail.com>
2020-06-25 15:31:13 +0100

    fetch data from the api

--- js/app.js
+++ js/app.js
@@ -1,10 +1,67 @@
+/*------------
+Global Variables
+-------------*/
+
+
+let employees = [];
+const urlAPI = 'https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone,dob &noinfo &nat=GB'
+const container = document.querySelector('.grid-container');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal-content');
+const closeModal = document.querySelector('.modal-close');
+
+//----------------------------------------------
+
+/*--------------
+Fetch info from API
+--------------*/
+fetch(urlAPI)
+    .then(res => res.json())
+    .then(res =>res.results)
+    .then(displayEmployees)
+    .catch(err => console.log(err))
+
+function displayEmployees(employeeData) {
+    employees = employeeData;
+
+    let employeeHTML = '';
+
+    employees.forEach((employee, index) => {
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employee.picture;
+
+        employeeHTML += `
+            <div class="card" data-index="${index}">
+                <img class="avatar" src="${picture.large}" />
+                <div class="text-container">
+                    <h2 class="name">${name.first} ${name.last}</h2>
+                    <p class="email">${email}</p>
+                    <p class="address">${city}</p>
+                </div>
+            </div>
+        `
+    });
+    container.innerHTML = employeeHTML;
+}
+
+
+
+/*------------
+Adds year to footer
+-------------*/
+
 let today = new Date();
 let year = today.getFullYear();
 let currentYear = document.querySelector('#date');
 currentYear.innerHTML = year;
 
 
+
+
 // Test to check modal works - to be removed
+/*
 const showModal = document.querySelector('#show');
 const closeModal = document.querySelector('.modal-close');
 
@@ -17,4 +74,5 @@ closeModal.addEventListener('click', e => {
     let modal = document.querySelector('.overlay');
     modal.classList.add('hidden');
 });
+*/
 
